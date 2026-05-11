963f6973ac705b3d42423495582101403dc7749a
Manuel Tornero <mjtf3@alu.ua.es>
2026-05-11 18:02:59 +0200

    cambiamos las tabs por nuevas paginas

--- appIonic/appIonic/src/app/app-routing.module.ts
+++ appIonic/appIonic/src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'wiki',
+    loadChildren: () => import('./wiki/wiki.module').then( m => m.WikiPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'exit',
+    loadChildren: () => import('./exit/exit.module').then( m => m.ExitPageModule)
   }
 ];
 @NgModule({
