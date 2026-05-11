29ad4e5f7f6993bf9e25b2e06ae39bb7d4296c4c
Manuel Tornero <mjtf3@alu.ua.es>
2026-05-11 18:27:38 +0200

    añadimos la info en cada pestaña

--- appIonic/appIonic/src/app/exit/exit.page.ts
+++ appIonic/appIonic/src/app/exit/exit.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { App } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-exit',
   templateUrl: './exit.page.html',
   styleUrls: ['./exit.page.scss'],
+  standalone: false,
 })
 export class ExitPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async confirmExit(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Confirmar salida',
+      message: '¿Seguro que quieres salir de la aplicación?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Salir',
+          role: 'destructive',
+          handler: () => {
+            if (Capacitor.isNativePlatform()) {
+              App.exitApp();
+            }
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
 }
--- appIonic/appIonic/src/app/wiki/wiki.page.ts
+++ appIonic/appIonic/src/app/wiki/wiki.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-wiki',
-  templateUrl: './wiki.page.html',
-  styleUrls: ['./wiki.page.scss'],
+  selector: "app-wiki",
+  templateUrl: "./wiki.page.html",
+  styleUrls: ["./wiki.page.scss"],
+  standalone: false,
 })
 export class WikiPage implements OnInit {
+  readonly categories: string[] = ["People", "Planets", "Species", "Starships"];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
